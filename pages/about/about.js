c341ed8ac7f1960db7cc73f06a483c2007ad9ca5
unknown <1594744173@qq.com>
2020-11-27 23:38:02 +0800

    first commit

---
+++ pages/about/about.js
@@ -0,0 +1,75 @@
+// pages/about/about.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImg: [
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1435.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1436.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1437.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1438.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1439.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1440.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1441.jpg" },
+      { src: "cloud://huize-6gnd35vr892a3802.6875-huize-6gnd35vr892a3802-1304009733/photos/IMG_1442.jpg" }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,39 @@
+var app = getApp()
+Page({
+  data: {
+    show: "",
+  },
+ 
+  onLoad: function () {
+    console.log('onLoad')
+  },
+  click: function () {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        this.show = wx.navigateTo({
+          url: '../skip/skip?title=' + res.result
+
+        })
+        that.setData({
+          show: this.show
+        })
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '失败',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      complete: (res) => {
+      }  
+    })
+  }
+})
